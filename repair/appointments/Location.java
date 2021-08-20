e695edaa25e7500a7694b3dcc91d4108dc1e887c
DociliaE <89230798+DociliaE@users.noreply.github.com>
2021-08-19 21:25:21 -0400

    Update Location.java

--- repair/appointments/Location.java
+++ repair/appointments/Location.java
@@ -5,4 +5,37 @@
 package repair.appointments;
 
 public class Location {
+  String address;
+  String city;
+  String state;
+  int zip;
+  
+  public void setAddress(String address){
+    this.address= address;
+  }
+  public void setCity(String city){
+    this.city= city;
+  }
+  public void setState(String state){
+    this.state= state;
+  }
+  public void setZip(int zip){
+    this.zip= zip;
+  }
+  public String getAddress(){
+    return address;
+  }
+  public String getCity(){
+    return city;
+  }
+  public String getState(){
+    return state;
+  }
+  public int getZip(){
+    return zip;
+  }
+  public String display(){
+    return address + city + state + zip;
+  }
+  
 }
